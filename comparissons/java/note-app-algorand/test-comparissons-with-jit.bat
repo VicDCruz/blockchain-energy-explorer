3ee0619e0eee296659285341fe3ba00ab4fe7d31
Victor Cruz <vicdan215@gmail.com>
2023-04-14 09:51:55 -0600

    Change classpath to exec test

--- comparissons/java/note-app-algorand/test-comparissons-with-jit.bat
+++ comparissons/java/note-app-algorand/test-comparissons-with-jit.bat
@@ -2,5 +2,5 @@ ECHO Start of Loop
 
 FOR /L %%i IN (1, 1, 100) DO (
     ECHO %%i
-    CALL "C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe" -file note-app-jit\results-%%i.csv -cmd C:\Users\vicda\.jdks\graalvm-ce-19\bin\java.exe -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler -cp .\target\proof-of-work-1.0-SNAPSHOT-jar-with-dependencies.jar org.example.Main
+    CALL "C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe" -file note-app-jit\results-%%i.csv -cmd C:\Users\vicda\.jdks\graalvm-ce-19\bin\java.exe -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler -cp .\target\note-app-algorand-1.0-SNAPSHOT-jar-with-dependencies.jar mx.unam.cruz.victor.notes.NoteField
 )
