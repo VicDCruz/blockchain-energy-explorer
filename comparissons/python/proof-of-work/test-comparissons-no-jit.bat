36351eb7b5b256e84f3b9a5cccda3bf89ea1c46e
Victor Cruz <vicdan215@gmail.com>
2023-04-14 08:38:07 -0600

    Add scripts to loop energy analysis

---
+++ comparissons/python/proof-of-work/test-comparissons-no-jit.bat
@@ -0,0 +1,6 @@
+ECHO Start of Loop
+
+FOR /L %%i IN (1, 1, 20) DO (
+    ECHO %%i
+    CALL "C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe" -file pow-no-jit\results-%%i.csv -cmd python .\main.py
+)
