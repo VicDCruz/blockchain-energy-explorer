27ffd18925833c13dbe927aac766e21c43b7276f
Victor Cruz <vicdan215@gmail.com>
2023-04-14 08:51:59 -0600

    Change loop to run analysis

--- comparissons/java/note-app-algorand/test-comparissons-no-jit.bat
+++ comparissons/java/note-app-algorand/test-comparissons-no-jit.bat
@@ -1,6 +1,6 @@
 ECHO Start of Loop
 
-FOR /L %%i IN (1, 1, 20) DO (
+FOR /L %%i IN (1, 1, 100) DO (
     ECHO %%i
     CALL "C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe" -file note-app-no-jit\results-%%i.csv -cmd C:\Users\vicda\.jdks\graalvm-ce-19\bin\java.exe -cp .\target\note-app-algorand-1.0-SNAPSHOT-jar-with-dependencies.jar mx.unam.cruz.victor.notes.NoteField
 )
--- comparissons/python/algorand-transaction-note/test-comparissons-with-jit.bat
+++ comparissons/python/algorand-transaction-note/test-comparissons-with-jit.bat
@@ -1,6 +1,6 @@
 ECHO Start of Loop
 
-FOR /L %%i IN (1, 1, 20) DO (
+FOR /L %%i IN (1, 1, 100) DO (
     ECHO %%i
     CALL "C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe" -file note-app-jit\results-%%i.csv -cmd C:\Program Files\pypy3.9\pypy.exe .\main.py
 )
