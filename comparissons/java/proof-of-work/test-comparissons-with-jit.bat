64eac80aa189d5bff9a741ded858d706b69d18c3
Victor Cruz <vicdan215@gmail.com>
2023-04-14 08:30:34 -0600

    Add scripts to loop energy analysis

---
+++ comparissons/java/proof-of-work/test-comparissons-with-jit.bat
@@ -0,0 +1,6 @@
+ECHO Start of Loop
+
+FOR /L %%i IN (1, 1, 20) DO (
+    ECHO %%i
+    CALL "C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe" -file results-%%i.csv -cmd C:\Users\vicda\.jdks\graalvm-ce-19\bin\java.exe -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler -cp .\target\proof-of-work-1.0-SNAPSHOT-jar-with-dependencies.jar org.example.Main
+)
