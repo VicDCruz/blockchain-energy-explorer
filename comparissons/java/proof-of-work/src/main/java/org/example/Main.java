bcd3ca0b1a76c16bd66383500adf844a72b6b403
Victor Cruz <vicdan215@gmail.com>
2023-04-17 08:42:35 -0600

    Use static import

--- comparissons/java/proof-of-work/src/main/java/org/example/Main.java
+++ comparissons/java/proof-of-work/src/main/java/org/example/Main.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import org.apache.commons.codec.digest.DigestUtils;
-
-import java.security.NoSuchAlgorithmException;
+import static org.apache.commons.codec.digest.DigestUtils.sha256Hex;
 
 public class Main {
     public static int nonce = 0;
     public static String data = "Hello, world!";
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) {
         mine(5, "e7wM2iSaaNssTJcHZRGaZYTEoSibEm6sqDdNm");
     }
 
@@ -27,6 +25,6 @@ public class Main {
 
     private static String calculateHash(String genesisHash, long millis) {
         String dataToHash = genesisHash + millis + nonce + data;
-        return DigestUtils.sha256Hex(dataToHash);
+        return sha256Hex(dataToHash);
     }
 }
