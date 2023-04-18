8ad58d40766dcaf8a4aa02f7e0225dd99be257a8
Victor Cruz <vicdan215@gmail.com>
2023-04-18 17:53:28 -0600

    Add new bash scripts in other Java projects

--- comparissons/kotlin/proof-of-work/test-comparissons-no-jit.bat
+++ comparissons/kotlin/proof-of-work/test-comparissons-no-jit.bat
@@ -2,5 +2,5 @@ ECHO Start of Loop
 
 FOR /L %%i IN (1, 1, 100) DO (
     ECHO %%i
-    CALL "C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe" -file pow-no-jit\results-%%i.csv -cmd C:\Users\vicda\.jdks\graalvm-ce-19\bin\java.exe -cp .\target\proof-of-work-1.0-SNAPSHOT-jar-with-dependencies.jar org.example.Main
+    CALL "C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe" -file pow-no-jit\results-%%i.csv -cmd C:\Users\vicda\.jdks\graalvm-ce-19\bin\java.exe -cp .\target\proof-of-work-1.0-SNAPSHOT.jar org.codehaus.mojo.MainKt
 )
