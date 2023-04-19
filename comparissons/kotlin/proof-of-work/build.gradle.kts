8c4b4f983d15b3885b680c3cdf66de5f54224311
Victor Cruz <vicdan215@gmail.com>
2023-04-19 08:33:46 -0600

    Update PoW Kotlin with Gradle

---
+++ comparissons/kotlin/proof-of-work/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("commons-codec:commons-codec:1.15")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
---
+++ comparissons/kotlin/proof-of-work/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "proof-of-work"
+
