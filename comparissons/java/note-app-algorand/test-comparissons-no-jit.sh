277e356117146d33f48d358b01364d42fefcab7f
Victor Cruz <vicdan215@gmail.com>
2023-04-18 18:20:11 -0600

    Updat bash scripts

--- comparissons/java/note-app-algorand/test-comparissons-no-jit.sh
+++ comparissons/java/note-app-algorand/test-comparissons-no-jit.sh
@@ -1,3 +1,3 @@
 for i in {1..100} ; do
-    /Applications/Intel\ Power\ Gadget/Powerlog -file pow-no-jit/results-"$i".csv -cmd /Users/vicdan/Library/Java/JavaVirtualMachines/graalvm-ce-19/Contents/Home/bin/java -cp ./target/note-app-algorand-1.0-SNAPSHOT-jar-with-dependencies.jar mx.unam.cruz.victor.notes.NoteField
+    /Applications/Intel\ Power\ Gadget/Powerlog -file note-app-no-jit/results-"$i".csv -cmd /Users/vicdan/Library/Java/JavaVirtualMachines/graalvm-ce-19/Contents/Home/bin/java -cp ./target/note-app-algorand-1.0-SNAPSHOT-jar-with-dependencies.jar mx.unam.cruz.victor.notes.NoteField
 done
