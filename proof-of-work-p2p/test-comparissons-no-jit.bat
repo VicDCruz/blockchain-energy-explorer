a038a9e0d49a7b5e20c34d5fa36c805e8ca10450
Victor Cruz <vicdan215@gmail.com>
2023-04-26 01:18:10 -0600

    Add test scripts

---
+++ proof-of-work-p2p/test-comparissons-no-jit.bat
@@ -0,0 +1,6 @@
+ECHO Start of Loop
+
+FOR /L %%i IN (1, 1, 100) DO (
+    ECHO %%i
+    CALL "C:\Program Files\Intel\Power Gadget 3.6\PowerLog3.0.exe" -file pow-no-jit\results-%%i.csv -cmd docker-compose up --scale pow=4
+)
