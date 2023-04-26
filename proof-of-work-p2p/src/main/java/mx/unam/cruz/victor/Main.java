86c5debccc8a85cb71148c08d6b9389c7f89af53
Victor Cruz <vicdan215@gmail.com>
2023-04-26 00:29:36 -0600

    Modify codebase to run with Docker

--- proof-of-work-p2p/src/main/java/mx/unam/cruz/victor/Main.java
+++ proof-of-work-p2p/src/main/java/mx/unam/cruz/victor/Main.java
@@ -4,17 +4,18 @@ import rice.environment.Environment;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.util.Arrays;
+import java.util.Optional;
 
 public class Main {
     // Command: /Users/victor.cruz/Library/Java/JavaVirtualMachines/corretto-18.0.2/Contents/Home/bin/java -jar target/proof-of-work-p2p-1.0-SNAPSHOT-jar-with-dependencies.jar 9002
     public static void main(String[] args) throws Exception {
         Environment environment = new Environment();
         environment.getParameters().setString("nat_search_policy", "never");
-        int bindPort = Arrays.stream(args).mapToInt(Integer::parseInt).findFirst().orElse(9001);
+        int bindPort = Integer.parseInt(Optional.of(args[0]).orElse("9001")); // DHT port
         int bootPort = 9001; // hardcoded port to connect to other instances
 
-        InetAddress bootInetAddress = InetAddress.getByName(InetAddress.getLocalHost().getHostAddress()); // not localhost
+        String dhtAddress = Optional.of(args[1]).orElse(InetAddress.getLocalHost().getHostAddress());
+        InetAddress bootInetAddress = InetAddress.getByName(dhtAddress); // DHT address
         InetSocketAddress bootAddress = new InetSocketAddress(bootInetAddress, bootPort);
         System.out.println("InetAddress: " + bootInetAddress + ", Bind port: " + bindPort);
 
